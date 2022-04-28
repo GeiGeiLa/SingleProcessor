7ea6360e25e86d0dc63f027099778be52f3b963b
GeiGeiLa <kyokatsmin1909@hotmail.com>
2022-04-28 16:22:01 +0800

    Use consumer producer instead

--- convert_request_listener.py
+++ convert_request_listener.py
@@ -1,30 +1,24 @@
 """A daemon server listening video process requests.
 
 Listens to socket messages passed from the upload nodejs server.
-
 """
 
-
-
-
-from filelock import FileLock
 from datetime import datetime
 from pathlib import Path
 from typing import Union
 import socket
 import queue
-
-import long_process
-
+import threading
+import time
+import shutil
+import os
 
 host = 'localhost'
 port = 3520
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+max_queue_length = 20
 
 dirname = Path(__file__).parent
 
-video_to_process = queue.Queue()
 
 # Python Queue does not know the exact length of queue.
 # Must maintain the size with a variable. 
@@ -33,69 +27,81 @@ qSize = 0
 VIDEO_HEADER = ("VIDEO_")
 TERMINATE_HEADER = ("TERMINATE_")
 
+exitFlag = False
+video_to_process = queue.Queue(max_queue_length)
 
-def call_detector(file_name):
-    long_process.perform_long_operation(file_name)
-    print("Completed")
+class Producer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = video_to_process
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == "__main__":
+    def run(self):
+        print("Start listening to node messages")
+        self.socket.bind((host, port))
+        self.socket.listen(max_queue_length)
+        while not exitFlag:
+            conn, addr = self.socket.accept()
 
-    s.bind((host, port))
-    s.listen(10)
-    print(f"Listening on port {port}")
-
-    try:
-        while True:
-
-            conn, addr = s.accept()
             request_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
             print(f"New request received at {request_time}")
 
             # Rejects non-localhost requests for protection.
             if '127.0.0.1' not in str(addr):
-                conn.send(str(f"Rejected connection: {str(addr)}").encode())
+                reject_message = f"Rejected connection: {str(addr)}"
+                print(reject_message)
+                conn.send(reject_message.encode())
                 conn.close()
                 continue
 
             indata = conn.recv(1024)
             if not indata:
+                print("indata is empty, skippped.")
                 continue
+
             indata = indata.decode()
 
             # Only accepts messages starting with "VIDEO_".
             if indata.startswith(VIDEO_HEADER):
+                # Get the video name
                 indata = indata[len(VIDEO_HEADER):]
-                print("Added video to queue")
-                video_to_process.put(indata)
-                qSize += 1
+                print(f"Added video {indata} to queue")
+                self.queue.put(indata)
                 conn.close()
-                print(video_to_process)
-                outdata = f'queuing {indata}'
-                # this is the only video to process
-                if qSize == 1:
-                    call_detector(video_to_process.get())
-                    qSize -= 1
-                
-            # Notifies that a video process is completed.
-            elif indata.startswith(TERMINATE_HEADER):
-                outdata = "Understood"
-                conn.send(outdata.encode())
-                conn.close()
-                if not video_to_process.empty():
-                    call_detector(video_to_process.get())
-                    qSize -= 1
+
             else:
                 outdata = f"Rejected connection due to invalid message: {indata}"
-                conn.send(outdata.encode())
+                print(outdata)
                 conn.close()
-    finally:
-        s.close()
-
-        # with FileLock(dirname / "lockfile") as lock:
-        #     # This part of code is guarenteed to be run by only a single thread
-        #     now = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-        #     print(f"Starting request at {now}")
-        #     long_process.perform_long_operation()
 
 
+class Consumer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = video_to_process
 
+    def make_bones(self, video_path: os.PathLike[str]):
+        new_folder = dirname / "processed_video_storage"
+        video_name = os.path.basename(video_path)
+        print(f"NEW VID {video_name}")
+        shutil.copyfile(video_path, new_folder / video_name)
+
+    def run(self):
+        print("Start consuming the video queue...")
+        while not exitFlag:
+            video_path = self.queue.get()
+            # print(f"Processing {video_path}")
+            self.make_bones(video_path)
+            print(f"Completed bone for: {video_path}")
+
+
+
+
+if __name__ == "__main__":
+    producer = Producer()          # create producer object with priority 6
+    consumer = Consumer()          # create consumer object with priority 2
+
+    # start producer and consumer threads
+    producer.start()
+    consumer.start()
+    pass
--- upload_video.js
+++ upload_video.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path')
 const formidable = require('formidable');
-
+const randomWords = require('random-words');
 const net = require('net');
 
 const HOST = '127.0.0.1';
@@ -11,20 +11,16 @@ const PORT = 3520;
 
 const app = express();
 
-function call_bone(files)
-{
-    console.log(files.TableTennisVideo.originalFilename);
-}
-
 const maxFileSizeInMB = 500;
 const allowedExtensions = ['jpg', 'jpeg', 'png', 'mp4', 'avi']
 
 app.post('/test', (req, res) =>{
+    let word = randomWords()
     let client = new net.Socket();
     client.connect(PORT, HOST, function()
     {
         console.log("Sending data to my Python server");
-        client.write(`VIDEO_${Date.now()}`);
+        client.write(`VIDEO_${word}`);
     });
 
     client.on('data', function(data){
@@ -36,44 +32,77 @@ app.post('/test', (req, res) =>{
     client.on('close', function() {
         console.log('Connection closed');
     });
-    res.send("Successfully uploaded");
+    res.send(word);
 });
 
-app.post('/upload', (req, res, next) => {
+app.post('/upload', (req, res) => {
     
     const form = new formidable.IncomingForm();
     form.maxFileSize = maxFileSizeInMB * 1024 * 1024;
-    form.parse(req, function(err, fields, files){
-        if (err)
-        {
-            res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
-            res.end(String(err));
-            return;        
-        }
-        // member of TableTennisVideo is for windows!!!
-        let oldPath = String(files.TableTennisVideo.filepath);
-        let newFileName = String(files.TableTennisVideo.originalFilename)
-        let extension = newFileName.substring(newFileName.lastIndexOf("."))
-        // reject invalid files
-        if (extension.length == 0 || !allowedExtensions.includes(extension.substring(1)))
-        {
-            fs.unlink(oldPath);
-            res.send("Rejected: uploaded disallowed files to this server.")
-            return;        
-        }
-        
-        newFileName = `${newFileName.substring(0,20)}_${Date.now()}${extension}`;
-        let newPath = path.join(__dirname, 'upload_storage')
-                + '/'+ newFileName
-        let rawData = fs.readFileSync(oldPath)
-      
-        fs.writeFile(newPath, rawData, function(err){
-            if(err) console.log(err)
-            console.log("Uploaded a new file " + newFileName)
-            res.send("Successfully uploaded")
-            //
-        })
-  })
+    try
+    {
+        form.parse(req, function(err, fields, files){
+            if (err)
+            {
+                res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+                res.end(String(err));
+                return;        
+            }
+            // member of TableTennisVideo is for windows!!!
+            let oldPath = String(files.TableTennisVideo.filepath);
+            let newFileName = String(files.TableTennisVideo.originalFilename)
+            let extension = newFileName.substring(newFileName.lastIndexOf("."))
+            // reject invalid files
+            if (extension.length == 0 || !allowedExtensions.includes(extension.substring(1)))
+            {
+                fs.unlink(oldPath);
+                res.send("Rejected: uploaded disallowed files to this server.")
+                return;        
+            }
+            // format: <shortenFileName>_<datetime>.<extension>
+            let filenameWithoutExt = newFileName.substring(0, newFileName.lastIndexOf('.'));
+            newFileName = `${filenameWithoutExt}_${Date.now()}${extension}`;
+            
+            // Sends socket messages to request process video
+            let newPath = path.join(__dirname, 'upload_storage')
+                    + '/'+ newFileName
+            let rawData = fs.readFileSync(oldPath)
+            
+          
+            fs.writeFile(newPath, rawData, function(err){
+                if(err) console.log(err)
+
+                let client = new net.Socket();
+                client.connect(PORT, HOST, function()
+                {
+                    console.log("Sending data to my Python server");
+                    client.write(`VIDEO_${newPath}`);
+                });
+            
+                client.on('data', function(data){
+                    console.log(`Data: ${data}`);
+                    client.destroy();
+                });
+            
+                // Add a 'close' event handler for the client socket
+                client.on('close', function() {
+                    console.log('Connection closed');
+                });
+
+                res.send("Successfully uploaded " + newFileName)
+                //
+            });
+        });
+    }
+    catch
+    {
+        res.send("Unknown error occurred while trying to upload.");
+    }
+});
+
+app.post('/download', (req, res) => {
+    // TODO: List out downloadable videos
+    res.send("The downloader will see you in the future!");
 });
    
 app.listen(3000, function(err){
